39d350883aab048c645fc3cef078c5f2492fc7a9
ByeongHunKim <rkwhr912@naver.com>
2023-04-19 13:36:05 +0900

    query Test V3 pool
    concentrate pool 에 등록되어있는 test pool 조회

---
+++ src/queryTestV3pool.ts
@@ -0,0 +1,15 @@
+import assert from 'assert'
+import {
+  ApiAmmV3PoolsItem,
+  ENDPOINT,
+} from '@raydium-io/raydium-sdk'
+import { RAYDIUM_MAINNET_API, TEST_TARGET_POOL } from '../config'
+import fetch from 'isomorphic-fetch';
+
+async function queryRoaV3Pool()  {
+  const ammV3Pools = (await fetch(ENDPOINT + RAYDIUM_MAINNET_API.ammV3Pools).then((res) => res.json())).data
+  const ammV3Pool = ammV3Pools.find((pool: ApiAmmV3PoolsItem) => pool.id === TEST_TARGET_POOL)
+  console.log('ammV3Pool',ammV3Pool)
+}
+
+queryRoaV3Pool()
